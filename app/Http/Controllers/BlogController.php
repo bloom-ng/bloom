203e9d2f94ec8115a1e18f80d4c4c57b27ec46ac
kingola1 <segun8427@gmail.com>
2024-10-30 09:32:15 +0100

    Added Views to blogs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,102 +8,111 @@ use Wink\WinkTag;
 
 class BlogController extends Controller
 {
-    
-    public function index(){
+
+    public function index()
+    {
 
         $posts = WinkPost::with('tags')
-                    ->live()
-                    ->orderBy('publish_date','DESC')
-                    ->paginate(15);
-        
-        
-        return view('blog.index',['posts'=>$posts, 'tags'=>WinkTag::all()]);
+            ->live()
+            ->orderBy('publish_date', 'DESC')
+            ->paginate(15);
+
+
+        return view('blog.index', ['posts' => $posts, 'tags' => WinkTag::all()]);
     }
 
-    public function search(Request $request){
-        $q= $request->input('post-search')===null?'':$request->input('post-search');
+    public function search(Request $request)
+    {
+        $q = $request->input('post-search') === null ? '' : $request->input('post-search');
         $posts = WinkPost::with('tags')
-                    ->where('title','LIKE','%'.$q.'%')
-                    ->live()
-                    ->orderBy('publish_date','DESC')
-                    ->paginate(15);
-        
+            ->where('title', 'LIKE', '%' . $q . '%')
+            ->live()
+            ->orderBy('publish_date', 'DESC')
+            ->paginate(15);
 
-        return view('post-search',['posts'=>$posts, 'tags'=>WinkTag::all(), 'query'=>$q,]);
+
+        return view('post-search', ['posts' => $posts, 'tags' => WinkTag::all(), 'query' => $q,]);
     }
 
-    
 
-    public function show($slug){
+
+    public function show($slug)
+    {
 
         $post = WinkPost::with('tags')
-                    ->live()
-                    ->whereSlug($slug)
-                    ->first();
-        
-        if(is_null($post)){ return redirect('/');}
+            ->live()
+            ->whereSlug($slug)
+            ->first();
+
+        if (is_null($post)) {
+            return redirect('/');
+        }
 
         $author = $post->author;
 
-       // $related_posts = $this->getRelatedPostsByTag($post->tags()->first());
+        // $related_posts = $this->getRelatedPostsByTag($post->tags()->first());
         $next_post = $this->getNextPost($post);
         $previous_post = $this->getPreviousPost($post);
-        
-        
-        return view('single_blog_post',['post' => $post, 
-                                  'tags'=>WinkTag::all(), 
-                                  'author'=>$author,
-                                  //'related_posts'=>$related_posts,
-                                  'next_post' => $next_post,
-                                  'previous_post' => $previous_post]);
+
+        $post->increment('views');
+
+
+        return view('single_blog_post', [
+            'post' => $post,
+            'tags' => WinkTag::all(),
+            'author' => $author,
+            //'related_posts'=>$related_posts,
+            'next_post' => $next_post,
+            'previous_post' => $previous_post
+        ]);
     }
 
-    public function tagPost($slug){
-       $posts = WinkTag::where('slug',$slug)
-                        ->first()
-                        ->posts()->with('tags')
-                        ->live()
-                        ->orderBy('publish_date','DESC')
-                        ->paginate(15);
+    public function tagPost($slug)
+    {
+        $posts = WinkTag::where('slug', $slug)
+            ->first()
+            ->posts()->with('tags')
+            ->live()
+            ->orderBy('publish_date', 'DESC')
+            ->paginate(15);
 
-        $tag = WinkTag::where('slug',$slug)->first();
-        
-      return view('filtered-posts',['posts'=>$posts, 'tags'=>WinkTag::all(), 'tag'=>$tag]);  
-        
+        $tag = WinkTag::where('slug', $slug)->first();
+
+        return view('filtered-posts', ['posts' => $posts, 'tags' => WinkTag::all(), 'tag' => $tag]);
     }
 
-    public function getRelatedPostsByTag($tag){
+    public function getRelatedPostsByTag($tag)
+    {
         /*
         *   Requires post with tags relations 
         *   and return related post
         */
-        
-        $posts = WinkTag::where('slug',$tag->slug)
-                        ->first()
-                        ->posts()->with('tags')
-                        ->live()
-                        ->orderBy('publish_date','DESC')
-                        ->take(8)
-                        ->get()->shuffle();
+
+        $posts = WinkTag::where('slug', $tag->slug)
+            ->first()
+            ->posts()->with('tags')
+            ->live()
+            ->orderBy('publish_date', 'DESC')
+            ->take(8)
+            ->get()->shuffle();
         return $posts;
     }
 
-    public function getNextPost($post){
+    public function getNextPost($post)
+    {
 
         $next_post = WinkPost::where('publish_date', '>', $post->publish_date)
-                                ->orderBy('publish_date','ASC')
-                                ->first();
-              return $next_post;              
-
+            ->orderBy('publish_date', 'ASC')
+            ->first();
+        return $next_post;
     }
 
-    public function getPreviousPost($post){
+    public function getPreviousPost($post)
+    {
 
         $previous_post = WinkPost::where('publish_date', '<', $post->publish_date)
-                                        ->orderBy('publish_date','DESC')
-                                        ->first();
-              return $previous_post;              
-
+            ->orderBy('publish_date', 'DESC')
+            ->first();
+        return $previous_post;
     }
-    
 }
